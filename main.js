caf3dd7da7e456ec305a46c26d962f6a2d47da8c
Iker Suen <suenik@oocl.com>
2018-11-21 08:53:43 +0800

    Completed Enhancement v2.0

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 function createMultiplyTable(start, end){
-	var result;
-	if(inputValidation(start, end)){
-		return doMultiplicationTable(start, end);
-	}else{
-		return null;
-	}
+	return inputValidation(start, end)?doMultiplicationTable(start, end):null
 }
 
 function inputValidation(start, end){
-	if(start > end || start > 1000 || start < 1 || end > 1000 || end < 1){
-			return false;
-	}else{
-		return true;
-	}
+	return (start > end || start > 1000 || start < 1 || end > 1000 || end < 1)?false:true
 }
 
 function doMultiplicationTable(start, end){
 	var requiredText = '';
 	for (var i = start; i <= end; i++) {
 			for(var j = start; j <= i; j++) {
-				var currentElement = lengthOfOneMulitplyElement(i,j);
-				var lastElement = lengthOfOneMulitplyElement(j, end);
-				requiredText += j + '*' + i + '=' + MultiplyTwoValue(i,j) + howManySpaceToAlign(elementLengthDifferent(lastElement, currentElement));
-
+				requiredText += j + '*' + i + '=' + MultiplyTwoValue(i,j) + howManySpaceToAlign(elementLengthDifferent(lengthOfOneMulitplyElement(j, end), lengthOfOneMulitplyElement(i,j)));
 			}
 			requiredText += "\n"
 	}
@@ -42,39 +30,12 @@ function howManySpaceToAlign(lengthDifferent){
 }
 
 function lengthOfOneMulitplyElement(element_start, element_end){
-	var basicLength = 2;
-	if(element_start < 10){
-		basicLength += 1;
-	}else if(element_start < 100){
-		basicLength += 2;
-	}else if(element_start < 1000){
-		basicLength += 3;
-	}else if(element_start === 1000){
-		basicLength += 4;
-	}else{
-	}
-
-	if(element_end < 10){
-		basicLength += 1;
-	}else if(element_end < 100){
-		basicLength += 2;
-	}else if(element_end < 1000){
-		basicLength += 3;
-	}else if(element_end === 1000){
-		basicLength += 4;
-	}else{
-	}
-
-	basicLength += MultiplyTwoValue(element_start, element_end).toString().length;
-
-	return basicLength;
+	return 2 + element_start.toString().length + element_end.toString().length + MultiplyTwoValue(element_start, element_end).toString().length;
 
 }
 
 function MultiplyTwoValue(first, second){
 	return first * second;
 }
-
-console.log(createMultiplyTable(998,1000));
 
 module.exports = {createMultiplyTable, inputValidation};
